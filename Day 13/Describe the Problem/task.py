1dee8b958ce7c1993e5921b09ebe629214c68cb6
Cleverson Fernando Rodrigues-CLEVERFR <cleverfr@motorola.com>
2025-02-02 11:47:02 -0300

    Day 13

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,10 @@
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it")
+
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -7,6 +12,10 @@ def my_function():
 my_function()
 
 # Describe the Problem - Write your answers as comments:
+# The frase "you got it" is not being printed
 # 1. What is the for loop doing?
+# It is not printing the frase
 # 2. When is the function meant to print "You got it"?
+# When the vlue of "i" becomes iqual 20
 # 3. What are your assumptions about the value of i?
+# It is not reaching the value os 20
--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,15 @@
-age = int(input("How old are you?"))
-if age > 18:
-print("You can drive at age {age}.")
+def check_age():
+    is_a_number = False
+    while not is_a_number:
+        try:
+            age = int(input("How old are you?"))
+            is_a_number = True
+        except ValueError:
+            print("You have typed a invalid number!")
+            is_a_number = False
+
+    if age > 18:
+        print(f"You can drive at age {age}.")
+
+
+check_age()
