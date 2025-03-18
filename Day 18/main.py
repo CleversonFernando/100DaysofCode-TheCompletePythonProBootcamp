11c7d5799182bc55e23d53b4741aa3a7b44b1955
Cleverson Fernando Rodrigues-CLEVERFR <cleverfr@motorola.com>
2025-03-18 19:36:34 -0300

    Day 18

---
+++ Day 18/main.py
@@ -0,0 +1,36 @@
+import random
+import turtle as t
+
+tim = t
+tim.shape("turtle")
+tim.color("green")
+#
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+#
+# print(heroes.gen())
+# print(villains.gen())
+
+# for _ in range(30):
+#     t.forward(10)
+#     t.penup()
+#     t.forward(10)
+#     t.pendown()
+angules = 3
+circule = 360
+list_of_colors = ["red", "blue", "Black", "purple", "pink", "orange", "yellow", "gray"]
+
+for _ in range(8):
+    angle_degree = int(circule / angules)
+    for _ in range(angules):
+        tim.forward(100)
+        tim.right(angle_degree)
+
+    angules += 1
+    new_color = random.choice(list_of_colors)
+    list_of_colors.remove(new_color)
+    tim.color(new_color)
+
+screen = t.Screen()
+screen.exitonclick()
