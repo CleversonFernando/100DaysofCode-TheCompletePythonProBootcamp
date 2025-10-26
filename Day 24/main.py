b4855e7e5b44f5bb04ff5ba5941c9d3681e9781b
Cleverson Fernando Rodrigues-CLEVERFR <cleverfr@motorola.com>
2025-10-26 17:02:29 -0300

    Day 24

--- Day 24/main.py
+++
@@ -1,9 +0,0 @@
-# with open("my_file.txt") as file:
-#     content = file.read()
-#
-#     print(content)
-from os import write
-
-with open("new_my_file.txt", mode="w") as file:
-    file.write("\nhello 3")
-
